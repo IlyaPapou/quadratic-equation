a819fc77f48c013b5f09437f444a703f5a7f1cf2
IlyaVento <ilya.popov.sbmt@gmail.com>
2018-02-25 20:51:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
   // your implementation
-}
+    let a, b, c;
+    let answer = [0, 0];
+    
+    equation = equation.replace(/\s/g, '');
+
+    a = equation.match(/[^\^]\d+/g)[0];
+    b = equation.match(/[^\^]\d+/g)[1];
+    c = equation.match(/[^\^]\d+/g)[2];
+
+    answer[0] = Math.round(((-1)*b+Math.sqrt(b*b-4*a*c))/(2*a)) ;
+    answer[1] = Math.round(((-1)*b-Math.sqrt(b*b-4*a*c))/(2*a)) ;
+    return answer.sort((x,y) => x-y);
+};
